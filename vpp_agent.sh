da37a06d19270771dd53a59e33f5ca760900b758
Shiv <shiv@kuberack.net>
2021-09-24 10:05:19 +0000

    - The agent waits for 60 secs for VPP to come up  - The mac address for the gateway is set  - Once the icmp firewall rule is added to the GCP VPC, able    to ping the VMs from external clients

--- vpp_agent.sh
+++ vpp_agent.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Check if vpp is up. Wait for a minute
+wait_time=60
+command="vppctl show interface eth0"
+until [ $wait_time -eq 0 ] || $command; do
+  sleep 1s
+  ((wait_time--))
+done
+
+# Check why we exited the loop
+if $command ; then
+  echo "Success"
+else
+  echo "Fail"
+  exit 1
+fi
+
 #get the ipaddress associated with host name
 hostname=$(hostname)
 node_ip=$(grep $hostname /config/node/ip | awk -F'=' '{print $2}')
@@ -10,8 +26,25 @@ vppctl set interface state eth0 up
 #setup the ip address
 vppctl set interface ip address eth0 $node_ip
 
-#setup the gateway ip
+#get the gateway IP from the config map
 gateway=$(grep gateway /config/node/ip | awk -F'=' '{print $2}')
+
+#generate the mac address for the gateway
+p1=$(echo $gateway |  awk -F'.' '{print $1}')
+p2=$(echo $gateway |  awk -F'.' '{print $2}')
+p3=$(echo $gateway |  awk -F'.' '{print $3}')
+p4=$(echo $gateway |  awk -F'.' '{print $4}')
+echo $p2 $p3 $p4
+h1=$(printf "%02x" $p1)
+h2=$(printf "%02x" $p2)
+h3=$(printf "%02x" $p3)
+h4=$(printf "%02x" $p4)
+gateway_mac="42:01:$h1:$h2:$h3:$h4"
+
+#set up ip neighbor for the gateway
+vppctl set ip neighbor eth0 $gateway $gateway_mac
+
+#setup the default route
 vppctl ip route add 0.0.0.0/0 via $gateway
 
 #infinite loop
